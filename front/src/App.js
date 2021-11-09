218ba2d3eadf12be4010fe6a46d42b0e4490752d
Eduardo Perez <e.perez@cignuz.com>
2021-11-09 02:50:09 -0600

    Se elimina db

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          jotos <code>src/App.js</code> and save to reload.
+          Aprenda react <code>src/App.js</code>.
         </p>
         <a
           className="App-link"
