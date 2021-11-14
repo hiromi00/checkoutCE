c3a218b52d8e4b03bd6fdf14340431f6ccf59a2a
Isaac Castro <i.castro@cignuz.com>
2021-11-14 16:36:51 -0600

    inicializacion front

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./views/Login";
+import Home from "./views/Home";
+import { Routes, Route } from "react-router-dom";
+import Checkout from "./views/Checkout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Aprenda react <code>src/App.js</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
