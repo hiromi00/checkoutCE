406bc9ddb8787982139b4bbcd1f0ccd139fab55f
Eduardo Perez <e.perez@cignuz.com>
2021-11-14 23:43:55 -0600

    Mockup para mostrar cards y se añade el snackbar al agregar un producto al carrito

--- front/src/components/CardArticulo.js
+++ front/src/components/CardArticulo.js
@@ -6,48 +6,50 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import SnackBar from "../components/SnackBar";
+import { SnackbarProvider, useSnackbar } from "notistack";
 
-export default function CardArticulo() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
+function CardMui() {
+  const { enqueueSnackbar } = useSnackbar();
+  const handleClickVariant = (variant) => () => {
+    // variant could be success, error, warning, info, or default
+    enqueueSnackbar("Se ha agregado con éxito", { variant });
   };
   return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://images6.alphacoders.com/361/thumbbig-361483.webp"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Pug
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          sx={{ textTransform: "none" }}
+          onClick={handleClickVariant("success")}
+        >
+          <ShoppingCartIcon /> Agregar al carrito
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default function CardArticulo() {
+  return (
     <>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" sx={{ textTransform: "none" }}>
-            <ShoppingCartIcon /> Agregar al carrito
-          </Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-      <SnackBar open={open} handleClick={handleClick} />
+      <SnackbarProvider maxSnack={3}>
+        <CardMui />
+      </SnackbarProvider>
     </>
   );
 }
--- front/src/views/Catalog/index.js
+++ front/src/views/Catalog/index.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import CardArticulo from '../../components/CardArticulo'
+import { makeStyles } from "@mui/styles";
+import React from "react";
+import CardArticulo from "../../components/CardArticulo";
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "100%",
+    heigh: "100%",
+  },
+  cards: {
+    display: "flex",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+    flex: "1",
+  },
 
+  spaceCard: {
+    margin: "1em",
+  },
+}));
+
+const foo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const Catalog = () => {
-    
-    return (
-        <div>
-            <CardArticulo/>
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <div className={classes.cards}>
+        {foo.map((item, index) => (
+          <div className={classes.spaceCard}>
+            <CardArticulo />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Catalog
+export default Catalog;
